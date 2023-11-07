8bb683efd685af8d69bb8cfb76875a744a443f02
abrar <abrar.gatorden@gmail.com>
2023-11-07 03:43:01 -0800

    creat 2 buttons using html & css

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Abrar")
+
+
+/* Lesson 1 Exercise -javascript Basic*/
+/* 1a.use aler();to display 'Good Morning!' in apopup*/
+alert('Good Morning')
+/* 1b.Display your name in a popup. */
+alert('Abrar')
+/* 1c.Using math,calculate 10+5 in the console. */
+let a=10+5
+console.log(a)
+/* 1d.Calculate 20-5 in the Console. */
+let b=20-5
+console.log(b)
+/* 1e.calculate 2+2-5 in the console. */
+let y=2+2-5
+console.log(y)
+ 
+/* Lesson 2 Numbers Math
+ */
+
+
